774371ea40d6eb9c75ef3e075011bd0eb868d9a7
Cliosight <58256348+cliosight@users.noreply.github.com>
2024-11-05 16:40:18 +0530

    Update survey_tables.sql

--- json_examples/survey/survey_tables.sql
+++ json_examples/survey/survey_tables.sql
@@ -1,1 +1,16 @@
+CREATE TABLE `survey` ( `id` int NOT NULL AUTO_INCREMENT, `fullname` varchar(255) DEFAULT NULL, `email` varchar(255) DEFAULT NULL, `linkedin` varchar(255) DEFAULT NULL, `mobile_country_code` varchar(255) DEFAULT NULL, `mobile_number` varchar(255) DEFAULT NULL, `gender` varchar(255) DEFAULT NULL, `prompt_engg` varchar(255) DEFAULT NULL, `job_role` varchar(255) DEFAULT NULL, `timestamp` varchar(255) DEFAULT NULL, `location` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `skills` ( `id` int NOT NULL AUTO_INCREMENT, `skill_name` varchar(255) NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `products` ( `id` int NOT NULL AUTO_INCREMENT, `product_name` varchar(255) NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `skills_survey` ( `id` int NOT NULL AUTO_INCREMENT, `survey_id` int DEFAULT NULL, `skill_name` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `products_survey` ( `id` int NOT NULL AUTO_INCREMENT, `survey_id` int DEFAULT NULL, `product_name` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `respondent_cities` ( `id` int NOT NULL AUTO_INCREMENT, `city_name` varchar(255) DEFAULT NULL, `survey_id` int DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `functional_jobs` ( `id` int NOT NULL AUTO_INCREMENT, `job` varchar(255) DEFAULT NULL, `experience` varchar(255) DEFAULT NULL, `survey_id` int NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `technical_jobs` ( `id` int NOT NULL AUTO_INCREMENT, `job` varchar(255) DEFAULT NULL, `experience` varchar(255) DEFAULT NULL, `survey_id` int NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
